7454af3c4e50725e0f69a1ca3a32234bc14abf82
Nicolas Torres <nicolastorres55@gmail.com>
2018-04-10 20:46:10 +0200

    delete System Volume Information and Recycle.bin folders from Data on startup

---
+++ i3/delete_win_folders.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -d "/mnt/Data/System Volume Information" ]; then
+    rm -r "/mnt/Data/System Volume Information"
+
+if [ -d "/mnt/Data/\$RECYCLE.BIN" ]; then
+    rm -r "/mnt/Data/\$RECYCLE.BIN"
