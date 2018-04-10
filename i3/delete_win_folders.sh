eb0f0da3363bf3e7898670a675c0f8afb6a64aaa
Nicolas Torres <nicolastorres55@gmail.com>
2018-04-10 20:51:59 +0200

    fixed syntax

--- i3/delete_win_folders.sh
+++ i3/delete_win_folders.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
 if [ -d "/mnt/Data/System Volume Information" ]; then
     rm -r "/mnt/Data/System Volume Information"
+fi
 
 if [ -d "/mnt/Data/\$RECYCLE.BIN" ]; then
     rm -r "/mnt/Data/\$RECYCLE.BIN"
+fi
+
