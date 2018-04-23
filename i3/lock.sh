ab848dae408ec747c3c3fbfc2c420d21f014929c
Nicolas Torres <nicolastorres55@gmail.com>
2018-04-23 09:35:07 +0200

    moved i3lock command to own sh script

---
+++ i3/lock.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# i3 lock color
+# i3lock -e --color=$bground --ringvercolor=$darkcyanff --insidevercolor=$cyanff --ringwrongcolor=$darkredff --insidewrongcolor=$redff --ringcolor=$darkblueff --keyhlcolor=$whiteff --insidecolor=00000000 --linecolor=00000000 --textcolor=$darkblackff --bshlcolor=$whiteff --separatorcolor=00000000 --veriftext="Hmm..." --wrongtext="No." --textsize=40 --modsize=12
+
+# i3 lock fancy pixelate
+i3lock-fancy -p
+
