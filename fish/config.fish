95f2ca49195034c449bcc9a9fd132bb0b4ef174c
nt <nicolastorres55@gmail.com>
2018-05-26 11:01:23 +0200

    fixed config for new setup of i3-gnome-fallback

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,3 @@ set -gx BROWSER google-chrome-stable
 
 source ~/.config/fish/abbr.fish
 
-# start X on login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty > ~/.xorg.log ^&1
-    end
-end
-
