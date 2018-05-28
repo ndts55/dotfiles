e61ccac1c0312176d03747ed728e91e132ff92cd
nt <nicolastorres55@gmail.com>
2018-05-28 09:34:02 +0200

    start polybar on all monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,5 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep1; done
 
-polybar example &
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -r -c ~/.config/polybar/config & done
 
