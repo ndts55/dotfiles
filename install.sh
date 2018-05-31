de44062d575af9cbb88a90e2f57bbf9c5bbf4da0
nt <nicolastorres55@gmail.com>
2018-05-31 13:20:39 +0200

    install.sh prefix in output for easy viewing

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ function create_link {
 	# if dir and link or file just leave as is
 	if [ -L "$2" ]; then
 		if [ -d "$2" ]; then
-			echo "$2 is link to dir already"
+			echo "W $2 is link to dir already"
 		elif [ -f "$2" ]; then
-			echo "$2 is link to file already"
+			echo "W $2 is link to file already"
 		elif [ ! -e "$2" ]; then
-			echo "$2 links to non-existent file or dir"
+			echo "E $2 links to non-existent file or dir"
 		else
-			echo "$2 is a link already"
+			echo "E $2 is a link already"
 		fi
 		return
 	fi
@@ -18,16 +18,16 @@ function create_link {
 	# if doesn't exist just create link
 	if [ ! -e "$2" ]; then
 		ln -s "$1" "$2"
-		echo "created link from $1 to $2"
+		echo "S created link from $1 to $2"
 		return
 	fi
 
 	# exists and is either not dir or not link
 	# rename and link
 	mv "$2" $2_old
-	echo "renamed $2 to $2_old"
+	echo "I renamed $2 to $2_old"
 	ln -s "$1" "$2"
-	echo "created link from $1 to $2"
+	echo "S created link from $1 to $2"
 }
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
