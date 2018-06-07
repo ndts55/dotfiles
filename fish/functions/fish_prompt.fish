554a89f069ef9fccb2ef635359737b246ce25c2e
nt <nicolastorres55@gmail.com>
2018-06-08 01:00:50 +0200

    changed prompt to bright cyan bar, removed git info coloring

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,28 +1,36 @@
 function fish_prompt --description 'Write out the prompt'
 
 	set -g __fish_git_prompt_show_informative_status 1
-	set -g __fish_git_prompt_hide_untrackedfiles 1
-	set -g __fish_git_prompt_color_branch magenta --bold
-	set -g __fish_git_prompt_showupstream "informative"
+
+#	set -g __fish_git_prompt_color_branch magenta --bold
+
+	set -g __fish_git_prompt_showupstream "auto"
+
 	set -g __fish_git_prompt_char_upstream_ahead "↑"
+
 	set -g __fish_git_prompt_char_upstream_behind "↓"
+
 	set -g __fish_git_prompt_char_upstream_prefix ""
+
 	set -g __fish_git_prompt_char_stagedstate "●"
+#	set -g __fish_git_prompt_color_stagedstate yellow
+
 	set -g __fish_git_prompt_char_dirtystate "✚"
+#	set -g __fish_git_prompt_color_dirtystate blue
+
+	set -g __fish_git_prompt_hide_untrackedfiles 1
 	set -g __fish_git_prompt_char_untrackedfiles "…"
+#	set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+
 	set -g __fish_git_prompt_char_conflictedstate "✖"
+#	set -g __fish_git_prompt_color_invalidstate red
+
 	set -g __fish_git_prompt_char_cleanstate "✔"
-	set -g __fish_git_prompt_color_dirtystate blue
-	set -g __fish_git_prompt_color_stagedstate yellow
-	set -g __fish_git_prompt_color_invalidstate red
-	set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-	set -g __fish_git_prompt_color_cleanstate green #  --bold
+#	set -g __fish_git_prompt_color_cleanstate green --bold
 
     set -l last_status $status
 
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
+#	set -g __fish_prompt_normal (set_color normal)
 
     set -l color_cwd
     set -l prefix
@@ -41,9 +49,10 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     # PWD
-    set_color $color_cwd
+    set_color -b $color_cwd
+	set_color black
+	echo -n " "
     echo -n (prompt_pwd)
-    set_color normal
 
     printf '%s ' (__fish_vcs_prompt)
 
@@ -53,5 +62,6 @@ function fish_prompt --description 'Write out the prompt'
 
     echo -n "$suffix "
 
-    set_color normal
+	set_color normal
+	echo -n " "
 end
