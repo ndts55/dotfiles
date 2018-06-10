60712d54fe92c4b6f9fda069293f1cdd4eab2705
nt <nicolastorres55@gmail.com>
2018-06-10 21:29:10 +0200

    tabs to spaces

--- install.sh
+++ install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 function create_link {
-	# if dir and link or file just leave as is
-	if [ -L "$2" ]; then
-		if [ -d "$2" ]; then
-			echo "W $2 is link to dir already"
-		elif [ -f "$2" ]; then
-			echo "W $2 is link to file already"
-		elif [ ! -e "$2" ]; then
-			echo "E $2 links to non-existent file or dir"
-		else
-			echo "E $2 is a link already"
-		fi
-		return
-	fi
+    # if dir and link or file just leave as is
+    if [ -L "$2" ]; then
+        if [ -d "$2" ]; then
+            echo "W | $2 is link to dir already"
+        elif [ -f "$2" ]; then
+            echo "W |  $2 is link to file already"
+        elif [ ! -e "$2" ]; then
+            echo "E |  $2 links to non-existent file or dir"
+        else
+            echo "E |  $2 is a link already"
+        fi
+        return
+    fi
 
-	# if doesn't exist just create link
-	if [ ! -e "$2" ]; then
-		ln -s "$1" "$2"
-		echo "S created link from $1 to $2"
-		return
-	fi
+    # if doesn't exist just create link
+    if [ ! -e "$2" ]; then
+        ln -s "$1" "$2"
+        echo "S |  created link from $1 to $2"
+        return
+    fi
 
-	# exists and is either not dir or not link
-	# rename and link
-	mv "$2" $2_old
-	echo "I renamed $2 to $2_old"
-	ln -s "$1" "$2"
-	echo "S created link from $1 to $2"
+    # exists and is either not dir or not link
+    # rename and link
+    mv "$2" $2_old
+    echo "I |  renamed $2 to $2_old"
+    ln -s "$1" "$2"
+    echo "S |  created link from $1 to $2"
 }
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
