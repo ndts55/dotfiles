ea39cf8187a070f6136b51c3cf8a8cc55db731d0
nt <nicolastorres55@gmail.com>
2018-11-14 17:21:01 +0100

    fixed launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,7 +2,7 @@
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -r -c ~/.config/polybar/config & done
 
