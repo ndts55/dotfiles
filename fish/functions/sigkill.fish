e7e3ac5f74af89c4d93157f0e4069485c5893582
Nicolas Torres <nicolastorres@mailbox.org>
2019-05-06 17:27:22 +0200

    fixed sigkill function

--- fish/functions/sigkill.fish
+++ fish/functions/sigkill.fish
@@ -1,6 +1,6 @@
 function sigkill --description "send sigkill to process"
     for arg in $argv
-        for pid in (pidof $arg)
+        for pid in (string split " " -- (pidof $arg))
             kill -9 $pid
         end
     end
