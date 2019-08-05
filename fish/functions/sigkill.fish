c69cf2a381d4708b8f0b4a2a6dbfaaa46ebbe6a5
Nicolas Torres <nicolastorres@mailbox.org>
2019-08-05 10:41:31 +0200

    removed sigkill function because pkill exists

--- fish/functions/sigkill.fish
+++
@@ -1,7 +0,0 @@
-function sigkill --description "send sigkill to process"
-    for arg in $argv
-        for pid in (string split " " -- (pidof $arg))
-            kill -9 $pid
-        end
-    end
-end
