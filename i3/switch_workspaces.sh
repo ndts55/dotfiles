2ca567b2f0497f1d9bd616adbefda4a7af361256
Nicolas Torres <nicolastorres@mailbox.org>
2020-11-15 21:46:07 +0100

    removed unneeded shell script

--- i3/switch_workspaces.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-destination=$1
-current=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
-
-if [ destination != current ]; then
-    i3-msg "rename workspace $current to tmp; rename workspace $destination to $current; rename workspace tmp to $destination"
-fi
-
